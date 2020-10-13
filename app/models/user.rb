8e2ac39a27f6d38746e65c6915b6ae1ff003cb51
naocho <kirateiru831@gmail.com>
2020-10-13 11:07:10 +0900

    フォーム実装からアクティブストレージまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
   has_many :taxis
   has_many :comments
 
-  with_options presence: true do 
-  validates :nickname
-  validates :email,                    format: { with: /\A.+@.+\z/, message: '@を含めてください'}
-  validates :password ,      format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,100}+\z/, message: '半角英数字混合で8文字以上使用してください'}
-  end
+  # with_options presence: true do 
+  # validates :nickname
+  # validates :email,                    format: { with: /\A.+@.+\z/, message: '@を含めてください'}
+  # validates :password ,      format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,100}+\z/, message: '半角英数字混合で8文字以上使用してください'}
+  # end
 end
 
